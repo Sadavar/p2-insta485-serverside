e7a4c49a13c4418707d604e0405d6bba742ac4a0
Varun Sadasivam <sadavar05@gmail.com>
2024-09-16 19:58:41 -0400

    haha

--- insta485/views/index.py
+++ insta485/views/index.py
@@ -4,14 +4,27 @@ Insta485 index (main) view.
 URLs include:
 /
 """
-
 import flask
-
 import insta485
 
 
-@insta485.app.route("/")
+@insta485.app.route('/')
 def show_index():
     """Display / route."""
-    context = {}
+
+    # Connect to database
+    connection = insta485.model.get_db()
+
+    # Query database
+    logname = "awdeorio"
+    cur = connection.execute(
+        "SELECT username, fullname "
+        "FROM users "
+        "WHERE username != ?",
+        (logname, )
+    )
+    users = cur.fetchall()
+
+    # Add database info to context
+    context = {"users": users}
     return flask.render_template("index.html", **context)
