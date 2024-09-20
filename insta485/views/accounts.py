a054560faaa38524136beb11e51a5de6bdd9e3f4
Adhav Rajesh <adhavrajesh04@gmail.com>
2024-09-20 19:36:57 -0400

    login

--- insta485/views/accounts.py
+++ insta485/views/accounts.py
@@ -7,7 +7,7 @@ URLs include:
 import sys
 import flask
 import arrow
-from flask import request
+from flask import request, abort, session
 from pathlib import Path
 import uuid
 import hashlib
@@ -16,8 +16,6 @@ import insta485
 
 algorithm = 'sha512'
 salt = uuid.uuid4().hex
-# Logged-in user's username
-logname = "awdeorio"
 
 def get_hashed_password(password):
     hash_obj = hashlib.new(algorithm)
@@ -37,11 +35,15 @@ def show_create():
 
 @insta485.app.route("/accounts/delete/")
 def show_delete():
+    # Logged-in user's username
+    logname = session.get("logname")
     context = {"logname": logname}
     return flask.render_template("accounts_delete.html", **context)
 
 @insta485.app.route("/accounts/edit/")
 def show_edit():
+    # Logged-in user's username
+    logname = session.get("logname")
     connection = insta485.model.get_db()
     cur = connection.execute(
         "SELECT USERNAME, FULLNAME, EMAIL, FILENAME FROM users WHERE USERNAME = ?",
@@ -55,6 +57,8 @@ def show_edit():
 
 @insta485.app.route("/accounts/password/")
 def show_password():
+    # Logged-in user's username
+    logname = session.get("logname")
     context = {"logname": logname}
     return flask.render_template("accounts_password.html", **context)
 
@@ -65,32 +69,52 @@ def update_accounts():
     connection = insta485.model.get_db()
     # Default target to '/' if not provided
     target = request.args.get("target", "/")
-
-    username = request.form["username"]
-    fullname = request.form["fullname"]
-    email = request.form["email"]
-    password = request.form["password"]
-    filename = request.files["file"]
     operation = request.form["operation"]
 
-    password_db_string = get_hashed_password(password)
 
 
     if operation == "create":
+        username = request.form["username"]
+        fullname = request.form["fullname"]
+        email = request.form["email"]
+        password = request.form["password"]
+        filename = request.files["file"]
+        password_db_string = get_hashed_password(password)
+
         filename.save(Path.cwd() / "sql" / "uploads" / filename.filename)
         connection.execute(
             "INSERT INTO users (USERNAME, FULLNAME, EMAIL, PASSWORD, FILENAME) VALUES (?, ?, ?, ?, ?)",
             (username, fullname, email, password_db_string, filename.filename)
         )
+    elif operation == "login":
+        username = request.form["username"]
+        password = request.form["password"]
+        if username == "" or password == "":
+            abort(400)
+        password_db_string = get_hashed_password(password)
+        user = connection.execute(
+            "SELECT * FROM users WHERE USERNAME = ? AND PASSWORD = ?",
+            (username, password_db_string)
+        ).fetchone()
+        if user:
+            session["logname"] = username
+            return flask.redirect(target)
+        else:
+            abort(403)
     elif operation == "edit_account":
+        fullname = request.form["fullname"]
+        email = request.form["email"]
+        filename = request.files["file"]
+
         # Delete the old file
         old_filename = connection.execute(
             "SELECT FILENAME FROM users WHERE USERNAME = ?",
             (logname, )
         ).fetchone()
-        old_file_path = Path.cwd() / "sql" / "uploads" / old_filename["FILENAME"]
-        old_file_path.delete()
+        old_file_path = Path.cwd() / "sql" / "uploads" / old_filename['filename']
+        old_file_path.unlink()
         filename.save(Path.cwd() / "sql" / "uploads" / filename.filename)
+        # Update the user's information
         connection.execute(
             "UPDATE users SET FULLNAME = ?, EMAIL = ?, FILENAME = ? WHERE USERNAME = ?",
             (fullname, email, filename.filename, logname)
