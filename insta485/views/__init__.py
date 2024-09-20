63084ed01ae4b7febb12b9cde5ab9a6656def979
Adhav Rajesh <adhavrajesh04@gmail.com>
2024-09-20 00:22:41 -0400

    Added account stuff

--- insta485/views/__init__.py
+++ insta485/views/__init__.py
@@ -5,3 +5,4 @@ from insta485.views.uploads import get_upload
 from insta485.views.posts import show_post
 from insta485.views.explore import show_explore
 from insta485.views.users import show_user, show_followers, show_following
+from insta485.views.accounts import show_login, show_create, show_delete
